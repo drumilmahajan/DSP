ef3af190ce9c08e3a74c89c2da8d20e3a17a9aab
Drumil Mahajan <dm3804@nyu.edu>
2016-10-25 19:07:56 -0400

    Q2 added

---
+++ lab06/Q2/playAM_blocking_fixhh.py
@@ -0,0 +1,117 @@
+# playAM_blocking_fix.py
+# Play a mono wave file with amplitude modulation. 
+# This implementation reads and plays a block at a time (blocking)
+# and corrects for block-to-block angle mismatch.
+# Modified by Drumil Mahajan as a part of course work. 
+
+# f0 = 0      # Normal audio
+f0 = 2000    # 'Duck' audio
+f1 = 1000
+
+BLOCKSIZE = 64      # Number of frames per block
+
+import pyaudio
+import struct
+import wave
+import math
+from matplotlib import pyplot as plt
+import pylab
+# import numpy as np
+
+WIDTH = 2           # Number of bytes per sample
+CHANNELS = 1        # mono
+RATE = 16000        # Sampling rate (frames/second)
+DURATION = 10        # duration of processing (seconds)
+LEN = DURATION*RATE   # N : Number of samples to process
+
+# Turning on Interactive mode so plot gets updated
+
+plt.ion()
+fig = plt.figure(1)
+line, = plt.plot(y)
+plt.ylim(-32000,32000)
+plt.xlim(0, BLOCKSIZE)
+plt.xlabel("Time (n)")
+plt.show()
+
+def clip16( x ):    
+    # Clipping for 16 bits
+    if x > 32767:
+        x = 32767
+    elif x < -32768:
+        x = -32768
+    else:
+        x = x        
+    return int(x)
+
+fileName = "Stereo.wav"
+
+# Opening wave file to be written
+wavOut = wave.open(fileName , 'w')
+print("Writing in the wave file %s " % fileName)
+
+
+# Set parameters for the output file 
+wavOut.setnchannels(2)
+wavOut.setsampwidth(WIDTH)
+wavOut.setframerate(RATE)
+
+# Open audio stream
+p = pyaudio.PyAudio()
+stream = p.open(format = p.get_format_from_width(WIDTH),
+                channels = CHANNELS,
+                rate = RATE,
+                input = True,
+                output = False)
+
+                
+
+# Create block (initialize to zero)
+output_block = [0 for n in range(0, 2*BLOCKSIZE)]
+
+# Number of blocks in DURATION
+num_blocks = int(math.floor(LEN/BLOCKSIZE))
+
+# Initialize angle
+theta = 0.0
+theta1 = 0.0
+
+# Block-to-block angle increment
+theta_del = (float(BLOCKSIZE*f0)/RATE - math.floor(BLOCKSIZE*f0/RATE)) * 2.0 * math.pi
+theta_del1 = (float(BLOCKSIZE*f1)/RATE - math.floor(BLOCKSIZE*f1/RATE)) * 2.0 * math.pi
+
+print('* Playing...')
+
+# Go through the input from mic for time = DURATION
+for i in range(0, num_blocks):
+
+     # Get BLOCKSIZE number of frames from audio input (microphone)
+    input_string = stream.read(BLOCKSIZE)
+
+    # Convert binary string to tuple of numbers
+    input_tuple = struct.unpack('h'*BLOCKSIZE, input_string)
+
+    # Go through block
+    for n in range(0, BLOCKSIZE):
+        # Amplitude modulation  (f0 Hz cosine)
+        output_block[2*n] = clip16( input_tuple[n] * math.cos(2*math.pi*n*f0/RATE + theta) )
+        output_block[2*n+1] = clip16( input_tuple[n] * math.cos(2*math.pi*n*f1/RATE + theta1) )
+        # output_block[n] = input_tuple[n] * 1.0  # for no processing
+
+    # Set angle for next block
+    theta = theta + theta_del
+    theta1 = theta1 + theta_del1
+
+    # Convert values to binary string
+    output_string = struct.pack('h' * 2 * BLOCKSIZE, *output_block)
+
+    # Write binary string to audio output stream
+    wavOut.writeframes(output_string)
+
+print('* Done')
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+# Original file by Gerald Schuller, October 2013
--- lab06/Q2/test.py
+++ lab06/Q2/test.py
@@ -8,15 +8,13 @@
 # f0 = 0      # Normal audio
 f0 = 400    # 'Duck' audio
 
-BLOCKSIZE = 2500    # Number of frames per block
+BLOCKSIZE = 64      # Number of frames per block
 
 import pyaudio
 import struct
 import wave
 import math
-import matplotlib
 from matplotlib import pyplot as plt
-matplotlib.use('TkAgg')
 from matplotlib import animation as ani
 import pylab
 from myfunctions import clip16
@@ -37,6 +35,8 @@ print 'The sampling rate is {0:d} samples per second'.format(RATE)
 print 'Each sample is {0:d} bytes'.format(WIDTH)
 print 'The signal is {0:d} samples long'.format(LEN)
 print 'The signal has {0:d} channel(s)'.format(CHANNELS)
+
+
 
 
 
@@ -65,62 +65,69 @@ theta = 0.0
 theta_del = (float(BLOCKSIZE*f0)/RATE - math.floor(BLOCKSIZE*f0/RATE)) * 2.0 * math.pi
 
 # Turning on Interactive mode so plot gets updated
-
-plt.ion()
-
-
-
-
 fig = plt.figure(1)
-
 line_out, = plt.plot(output_block)
-#fig1 = plt.plot(output_block)[0]
-
 line_in, = plt.plot(input_int)
 plt.ylim(-32000,32000)
 plt.xlim(0, BLOCKSIZE)
 plt.xlabel("Time (n)")
 plt.show()
 
+
+
 print('* Playing...')
 
+
+    
+
+
 # Go through wave file 
-for i in range(0, num_blocks):
+def update():
+    global output_block
+    global input_int
+    global theta
+    global theta_del
+    global wf
+    global line_in
+    global line_out
 
-    # Get block of samples from wave file
-    input_string = wf.readframes(BLOCKSIZE)     # BLOCKSIZE = number of frames read
-    # Convert binary string to tuple of numbers    
-    input_tuple = struct.unpack('h' * BLOCKSIZE, input_string)
-            # (h: two bytes per sample (WIDTH = 2))
+    for n in range(0,num_blocks):
 
-    # Go through block
-    for n in range(0, BLOCKSIZE):
-        # Amplitude modulation  (f0 Hz cosine)
-        output_block[n] = input_tuple[n] * math.cos(2*math.pi*n*f0/RATE + theta)
-        input_int[n] = input_tuple[n]
-        #fig1.set_data(output_block)
         
-        # output_block[n] = input_tuple[n] * 1.0  # for no processing
-    
-    # Set angle for next block
-    theta = theta + theta_del
 
-    # Convert values to binary string
-    output_string = struct.pack('h' * BLOCKSIZE, *output_block)
+        # Get block of samples from wave file
+        input_string = wf.readframes(BLOCKSIZE)     # BLOCKSIZE = number of frames read
+        # Convert binary string to tuple of numbers    
+        input_tuple = struct.unpack('h' * BLOCKSIZE, input_string)
+                # (h: two bytes per sample (WIDTH = 2))
 
-    # Write binary string to audio output stream
-    stream.write(output_string)
+        # Go through block
+        for n in range(0, BLOCKSIZE):
+            # Amplitude modulation  (f0 Hz cosine)
+            output_block[n] = input_tuple[n] * math.cos(2*math.pi*n*f0/RATE + theta)
+            input_int[n] = input_tuple[n]
+            # output_block[n] = input_tuple[n] * 1.0  # for no processing
+        
+        # Set angle for next block
+        theta = theta + theta_del
 
-    # Plotting the graph
-    
-    #plt.pause(0.00000001)
-    line_out.set_ydata(output_block)
-    line_in.set_ydata(input_int)
-    plt.pause(0.001)
+        # Convert values to binary string
+        output_string = struct.pack('h' * BLOCKSIZE, *output_block)
 
-    
-    
+        # Write binary string to audio output stream
+        stream.write(output_string)
+        line_out.set_ydata(output_block)
+        line_in.set_ydata(input_int)
+        
+        
+        # Plotting the graph
+        #plt.pause(0.00000001)
 
+        
+        #plt.show()
+
+anim = ani.FuncAnimation(fig,update,frames=200, interval=50, blit=True)    
+plt.show()
 print('* Done')
 
 stream.stop_stream()
